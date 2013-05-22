25896a4ed857e9b3b03baee141f700d887d0f077
Bernardo Fontes <falecomigo@bernardofontes.net>
2013-05-22 20:34:25 -0300

    Adiciona esqueleto do projeto

---
+++ ops/ops/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for ops project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'ops'
+
+SPIDER_MODULES = ['ops.spiders']
+NEWSPIDER_MODULE = 'ops.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ops (+http://www.yourdomain.com)'
